0ffb5d2e48fdca537fe334e5e95f8a13b4ae4a95
ToniTani <toni.tani@student.saimia.fi>
2019-11-02 20:07:58 +0200

    added addValue function with eval and error message when inputting operator twice.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { CalculatorComponent } from './calculator/calculator.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -5,12 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
 
-  constructor() { }
-  buttons: string[] = [ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '' ];
+export class CalculatorComponent implements OnInit {
+  buttons: string[] = [ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '<', '0', '+', '=' ];
+  result = '';
+
+  private preValue = '';
+  private curValue = '';
 
   ngOnInit() {
   }
 
+  addValue(value: string) {
+    if  (this.result !== '') {
+    this.preValue = this.curValue;
+    this.curValue = value;
+    }
+    if ((this.preValue === '+' || this.preValue === '-' || this.preValue === '/' || this.preValue === '*' )
+      && (value === '+' || value === '-' || value === '/' ||  value === '*')) {
+      alert('error'); } // adds error if inputted same operator twice
+
+    if (value === 'C') {
+      this.result = '';
+    } else if (value === '<') {
+      this.result = this.preValue !== '=' ? this.result.slice(0, -1) : this.result;
+    } else if (value === '=') {
+      this.result = eval(this.result);  // not forbidden in this calculator
+    } else {
+      this.result += value;
+    }
+  }
 }
+
