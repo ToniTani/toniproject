6e684454650603e2f9bcc9dea9fed5f1f076ec2c
ToniTani <toni.tani@student.saimia.fi>
2019-11-04 14:40:11 +0200

    fixed functions alert message, added extra buttons.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CalculatorComponent implements OnInit {
-  buttons: string[] = [ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '<', '0', '+', '=', '.' ];
+  buttons: string[] = [ '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '<', '0', '+', '=', '.', '(', ')' ];
   result = '';
 
   private preValue = '';
@@ -23,7 +23,7 @@ export class CalculatorComponent implements OnInit {
     }
     if ((this.preValue === '+' || this.preValue === '-' || this.preValue === '/' || this.preValue === '*' || this.preValue === '.' )
       && (value === '+' || value === '-' || value === '/' ||  value === '*' || value === '.')) {
-      alert('error'); } // adds error if inputted same operator twice
+      alert('error, do not add extra operators'); } else { // adds error if inputted same operator twice
 
     if (value === 'C') {
       this.result = '';
@@ -33,7 +33,7 @@ export class CalculatorComponent implements OnInit {
       this.result = eval(this.result);  // not forbidden in this calculator
     } else {
       this.result += value;
-    }
+    }}
   }
 }
 
