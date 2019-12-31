385341ccc2e52cbd672ff2135a91db1d4877fa2a
ToniTani <toni.tani@student.saimia.fi>
2019-12-31 22:13:32 +0200

    fixed reactive form with style and better validation

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, Validators} from '@angular/forms';
-import {FormGroup} from '@angular/forms';
+import { MustMatch} from './match-validator';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
-import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
-declare var NgForm: any;
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-
 export class ReactiveFormComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  this.registerForm = this.fb.group({
-    firstName: ['keinu', [Validators.required, Validators.minLength(2)]],
-    lastName: ['tuoli', [Validators.required, Validators.minLength(2)]],
-    personId: [''],
-    emailAddress: ['jee@jee.com', [Validators.required, Validators.email]],
-    // accountDetails: this.fb.group({
-    username: ['', [Validators.required ]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    confirmPassword: ['', Validators.required],
-  });
-  }
-  get fval() {
-    return this.registerForm.controls;
-  }
-  onSubmit() {
-    // console.log(this.profileForm.value);
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')],
+      lastName: ['', Validators.required, Validators.minLength(2), Validators.pattern('[a-zA-Z ]*')],
+      social: ['', [Validators.required, Validators.pattern(
+        '(0[1-9]|[12]\\d|3[01])(0[1-9]|1[0-2])([5-9]\\d\\+|\\d\\d-|[01]\\dA)\\d{3}[\\dA-Z]$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(10), Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).*$')]],
+      confirmPassword: ['', Validators.required],
+      acceptTerms: [false, Validators.requiredTrue]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
   }
 
-  submit() {
+  // convenience getter for easy access to form fields
+  get form() { return this.registerForm.controls; }
+
+  onSubmit() {
     this.submitted = true;
+
+    // invalid message
     if (this.registerForm.invalid) {
       return;
     }
-    alert('form fields are validated successfully!');
+
+    // display form values on success
+    alert('SUCCESS!! :-D');
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 }
