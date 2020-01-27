dc34b0c00d35446230f597af3fd2d54856b6b9a0
ToniTani <toni.tani@student.saimia.fi>
2020-01-27 03:33:16 +0200

    Created new working login (auth) components for authenticated login. Changed styling to bootstrap instead of angular material. Created new AuthGuard, AuthService, user.model.ts and new routes.

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthResponseData, AuthService} from './auth.service';
+import {Observable} from 'rxjs';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent {
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+    authObs = this.authService.login(email, password);
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.router.navigate(['/admin']);
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
+  }
+}
