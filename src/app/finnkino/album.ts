f5beb5588171f952bf5afccc13b8153feaf1a6bb
ToniTani <toni.tani@student.saimia.fi>
2021-03-11 23:00:37 +0200

    trying to fix html layout

--- src/app/finnkino/album.ts
+++ src/app/finnkino/album.ts
@@ -2,13 +2,17 @@ export class Album {
   ID: number;
   Views: number;
   Title: string;
-  UserImageURL: string;
+  ImageURL: string;
+  PublishDate: string;
+  HTMLLead: string;
 
-  constructor(id: number, userImageURL: string, views: number, title: string, ) {
+  constructor(id: number, userImageURL: string, views: number, title: string, publishDate: string, htmlLead: string) {
     this.ID = id;
     this.Views = views;
     this.Title = title;
-    this.UserImageURL = userImageURL;
+    this.ImageURL = userImageURL;
+    this.PublishDate = publishDate;
+    this.HTMLLead = htmlLead;
   }
 }
 
