9c96f24a3ec62662e71284d009326d7f15d351fd
ToniTani <toni.tani@student.saimia.fi>
2021-03-12 01:13:20 +0200

    SpecFiles to Custom Schemas while testing in actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -101,6 +101,7 @@ import { AuthService} from './auth/auth.service';
 
   ],
   providers: [PokemonService, QuestionsService, AuthService],
-  bootstrap: [AppComponent, ]
+  bootstrap: [AppComponent, ],
+  schemas:   [ NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
